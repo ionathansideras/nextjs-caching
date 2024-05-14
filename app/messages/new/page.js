3055165cdc3349cba1bf5d5001bf4f384e8d1beb
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-14 22:09:05 +0300

    commit

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,29 +1,34 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
+import { addMessage } from "@/lib/messages";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
-  async function createMessage(formData) {
-    'use server';
+    async function createMessage(formData) {
+        "use server";
 
-    const message = formData.get('message');
-    addMessage(message);
-    redirect('/messages');
-  }
+        const message = formData.get("message");
+        addMessage(message);
+        // clear the cache for the messages page so we can see the new message
+        // revalidatePath("/messages");
+        // clears the cache for all the "msg" tags that are set on some fetch requests
+        revalidateTag("msg");
+        redirect("/messages");
+    }
 
-  return (
-    <>
-      <h2>New Message</h2>
-      <form action={createMessage}>
-        <p className="form-control">
-          <label htmlFor="message">Your Message</label>
-          <textarea id="message" name="message" required rows="5" />
-        </p>
+    return (
+        <>
+            <h2>New Message</h2>
+            <form action={createMessage}>
+                <p className="form-control">
+                    <label htmlFor="message">Your Message</label>
+                    <textarea id="message" name="message" required rows="5" />
+                </p>
 
-        <p className="form-actions">
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
-  );
+                <p className="form-actions">
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </>
+    );
 }
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,16 +1,24 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { unstable_noStore } from "next/cache";
+
+// settings for file cache configuration
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+    // this will disable the cache for this request
+    // unstable_noStore();
+    // we add a tag so we revalidate the cache for this tag when we create a new message
+    const response = await fetch("http://localhost:8080/messages", {
+        next: {
+            tags: ["msg"],
+        },
+    });
+    const messages = await response.json();
 
-  if (!messages || messages.length === 0) {
-    return <p>No messages found</p>;
-  }
+    if (!messages || messages.length === 0) {
+        return <p>No messages found</p>;
+    }
 
-  return <Messages messages={messages} />;
+    return <Messages messages={messages} />;
 }
