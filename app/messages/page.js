6fbb40d06c4f8d4b12fd0d39239f7f418bc13de2
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-14 22:38:35 +0300

    chore: Remove backend package.json and app.js

--- app/messages/page.js
+++ app/messages/page.js
@@ -7,15 +7,6 @@ import { unstable_noStore } from "next/cache";
 // export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-    // this will disable the cache for this request
-    // unstable_noStore();
-    // we add a tag so we revalidate the cache for this tag when we create a new message
-    // const response = await fetch("http://localhost:8080/messages", {
-    //     next: {
-    //         tags: ["msg"],
-    //     },
-    // });
-
     const messages = await getMessages();
 
     if (!messages || messages.length === 0) {
